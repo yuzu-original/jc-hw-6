913921e05a0049099e95c356e5d8d1dca3b4c4c7
YuZu <yurkazubkov@yandex.ru>
2023-07-03 16:35:23 +0300

    init

---
+++ src/main/java/course/jchw6/JcHw6Application.java
@@ -0,0 +1,13 @@
+package course.jchw6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcHw6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcHw6Application.class, args);
+    }
+
+}
---
+++ src/test/java/course/jchw6/JcHw6ApplicationTests.java
@@ -0,0 +1,13 @@
+package course.jchw6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcHw6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
